4271de8cd9fad9a3f11279a61ed0ef04a9f5d854
Beefsupreme21 <beefsupreme21@hotmail.com>
2022-06-29 21:59:30 -0600

    Update

--- To do Linethrough(complete)/todo.js
+++ To do Linethrough(complete)/todo.js
@@ -5,11 +5,10 @@ let inputField = document.getElementById('inputField');
 addToDoButton.addEventListener('click', function(){ // We can add an Event Listener to our button/. 
     var paragraph = document.createElement('p'); // Makes a <p> element
     paragraph.classList.add('paragraph-styling'); // Add the styling from CSS
-
     paragraph.innerText = inputField.value; // This will set the paragraph inner text to the value of the input field
     toDoContainer.appendChild(paragraph); // This will append the child to the paragraph
-    
     inputField.value = ""; // This will clear the input field once it's been added
+    
     paragraph.addEventListener('click', function(){
         paragraph.style.textDecoration = "line-through"; // On one click it will show the text as line through
     })
